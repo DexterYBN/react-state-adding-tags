58ebeee92fa108cbefa644237bd79f88eeec0787
DexterYBN <yakubov959@mail.ru>
2022-11-27 21:09:28 +0300

    #Domination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './styles.css'
 
 function App() {
+
+  const [addText, setAddText] = useState("")
+
+  const [arrayText, setArrayText] = useState([])
+
+  const [blur, setBlur] = useState(false)
+
+  const handleAddText = (e) => {
+    e.preventDefault()
+    setAddText(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setAddText("")
+    setArrayText([...arrayText, addText])
+    setBlur(false)
+  }
+
+  const handleXButton = (index) => {
+    setArrayText(arrayText.filter((item, i) => index !== i))
+  }
+
+  const handleBlur = () => {
+    setBlur(true)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form onSubmit={handleSubmit}>
+        <input
+          className={(!addText && blur) ? 'error' : 'inputAddText'}
+          type="text"
+          value={addText}
+          onChange={handleAddText}
+          onBlur={handleBlur}
+        />
+        <button
+          className='addTextButton'
+          type='submit'
+          disabled={!addText || addText[0] === " "}
         >
-          Learn React
-        </a>
-      </header>
+          Отправить</button>
+      </form>
+      {(!addText && blur) && <p className="fail">Поле ввода не должно быть пустым, чел</p>}
+      <span className='span'>
+        {arrayText.map((item, index) => {
+          return (
+            <li className='li' key={index}>
+              {item}
+              <button className='x'
+                onClick={() => handleXButton(index)}
+              >
+                ×
+              </button>
+            </li>
+          )
+        })}
+      </span>
     </div>
   );
 }
